d5957a202e6251b6237696f53d11713d3df2ed75
Nada Attia <nma46@cornell.edu>
2021-03-24 01:11:05 -0400

    Add starter code

---
+++ _build/command.mli
@@ -0,0 +1,7 @@
+(* type squares_move = Board.square * Board.square
+
+   type command = | Move of squares_move | Undo | Forfeit | Hint *)
+
+(* exception Illegal *)
+
+(* val parse : string -> command *)
---
+++ _build/state.mli
@@ -0,0 +1,12 @@
+type state
+
+(* val update_state : Board.t -> Command.command -> state *)
+
+(** [where_move board square] is a list of squares that the player can
+    legally move to from the [square] that they're currently on *)
+
+(* val where_move : Board.t -> Board.square -> Board.square list
+
+   val can_move : Board.t -> Board.color -> bool
+
+   val can_jump : Board.t -> Board.color -> bool *)
