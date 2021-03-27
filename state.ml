3b9d8b46cfc66b3f65e6472f29c801bcb5d5feb0
Adam Kadhim <ak779@cornell.edu>
2021-03-27 00:12:26 -0400

    wip

--- state.ml
+++ state.ml
@@ -17,7 +17,7 @@ let where_move board square = failwith "Unimplemented"
 let check_if_occupied square = if square.occupant = None then false else true
 
 let is_valid_label lbl = 
-  if Char.code (Board.label_hd lbl) > 96 and Char.code (Board.label_hd lbl) < 105 then 
+  if Char.code (Board.label_hd lbl) > 96 && Char.code (Board.label_hd lbl) < 105 then 
     if Board.label_tl < 9 and Board.label_hd > 0 then true 
     else false 
   else false
@@ -34,22 +34,22 @@ let get_movable_squares_reg square color board =
   let upper_bound = if 8 <= (Board.label_tl labl) + 2 then 8 else (Board.label_tl labl) + 2 in 
   
   (*if white...*)
-  let ahead
-  let square_ahead1 = Board.get_square labl board
-
-  
-  
-  for i = (Board.label_tl labl) + 1 to upper_bound do 
-    if check_if_occupied (Board.get_square labl board) then () else squares
-  done
+  if 8 <= (Board.label_tl labl) + 1 then 
+    let ahead1_label = (ltr_pos, num_pos + 1) in
+    let square_ahead1 = Board.get_square ahead1_label board in 
+    let squares = square_ahead1 :: squares in 
+    if 8 <= (Board.label_tl labl) + 2 then squares
+    else squares
+  else squares
 
 
 let can_move (square: Board.square) (board : Board.t) (st : state) = 
   let condition1 = not check_if_occupied in 
   let condition2 = square.occupant.color = state.turn in
-  let label = square.label in
+  let label = square.label 
+(* in
   if square.occupant.role = Man then
-    List.map get
+    List.map get *)
 
 
 
