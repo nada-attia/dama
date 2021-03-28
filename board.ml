5cea7963461e95fd3e0533a4da1b4623a14f195f
Adam Kadhim <ak779@cornell.edu>
2021-03-27 22:07:55 -0400

    wip

--- board.ml
+++ board.ml
@@ -68,32 +68,65 @@ let get_square labl (brd : t) =
   let col = Char.code col_ltr - 97 in
   List.nth row_lst col
 
-(* Helper function to get list of left square to [square] if exists,
-   else []. *)
-let square_left sq board =
+(* Helper function to get list of square left of [square] if exists,
+   else []. Behavior varies dependent on color since orientation of
+   board changes. Uses list c_val determined by color [color] to perform
+   operations. *)
+let square_left sq color board =
   let ltr_pos, num_pos = sq.label in
   let ltr_pos_num = Char.code ltr_pos in
-  if ltr_pos_num - 1 >= 97 then
-    let left1_label = (Char.chr (ltr_pos_num - 1), num_pos) in
+  let c_vals =
+    if color = White then [| ltr_pos_num - 1; 97; -1 |]
+    else [| 104; ltr_pos_num + 1; 1 |]
+  in
+  if c_vals.(0) >= c_vals.(1) then
+    let left1_label = (Char.chr (ltr_pos_num - c_vals.(2)), num_pos) in
     [ get_square left1_label board ]
   else []
 
-(* Helper function to get list of right square to [square] if exists,
-   else []. *)
-let square_right sq board =
+(* Helper function to get list of square right of [square] if exists,
+   else []. Behavior varies dependent on color since orientation of
+   board changes. Uses list c_val determined by color [color] to perform
+   operations. *)
+let square_right sq color board =
   let ltr_pos, num_pos = sq.label in
   let ltr_pos_num = Char.code ltr_pos in
-  if ltr_pos_num + 1 <= 104 then
-    let right1_label = (Char.chr (ltr_pos_num + 1), num_pos) in
+  let c_vals =
+    if color = White then [| ltr_pos_num + 1; 104; 1 |]
+    else [| 97; ltr_pos_num - 1; -1 |]
+  in
+  if c_vals.(0) <= c_vals.(1) then
+    let right1_label = (Char.chr (ltr_pos_num + c_vals.(2)), num_pos) in
     [ get_square right1_label board ]
   else []
 
 (* Helper function to get list of square above of [square] if exists,
-   else []. *)
-let square_above sq board =
+   else []. Behavior varies dependent on color since orientation of
+   board changes. Uses list c_val determined by color [color] to perform
+   operations. *)
+let square_above sq color board =
   let ltr_pos, num_pos = sq.label in
-  if num_pos + 1 <= 8 then
-    let above1_label = (ltr_pos, num_pos + 1) in
+  let c_vals =
+    if color = White then [| num_pos + 1; 8; 1 |]
+    else [| 1; num_pos - 1; -1 |]
+  in
+  if c_vals.(0) <= c_vals.(1) then
+    let above1_label = (ltr_pos, num_pos + c_vals.(2)) in
+    [ get_square above1_label board ]
+  else []
+
+(* Helper function to get list of square below of [square] if exists,
+   else []. Behavior varies dependent on color since orientation of
+   board changes. Uses list c_val determined by color [color] to perform
+   operations. *)
+let square_below sq color board =
+  let ltr_pos, num_pos = sq.label in
+  let c_vals =
+    if color = White then [| num_pos - 1; 1; 1 |]
+    else [| 8; num_pos + 1; -1 |]
+  in
+  if c_vals.(0) >= c_vals.(1) then
+    let above1_label = (ltr_pos, num_pos - c_vals.(2)) in
     [ get_square above1_label board ]
   else []
 
@@ -111,8 +144,8 @@ let is_valid_label lbl =
    on top of, left of, or right of square [square] from board [board],
    depending on which helper function [func] is passed in. If the square
    is vacant, it will be returned, otherwise None. *)
-let get_vacant func square board =
-  let res_sqs = func square board in
+let get_vacant func square color board =
+  let res_sqs = func square color board in
   try
     let res_sqr = List.nth res_sqs 0 in
     if check_if_occupied res_sqr then None else Some res_sqr
@@ -124,39 +157,42 @@ let get_vacant func square board =
 let get_movable_squares_reg square color board =
   let squares = [] in
   let squares =
-    if get_vacant square_above square board <> None then
-      get_vacant square_above square board :: squares
+    if get_vacant square_above square color board <> None then
+      get_vacant square_above square color board :: squares
     else squares
   in
   let squares =
-    if get_vacant square_right square board <> None then
-      get_vacant square_right square board :: squares
+    if get_vacant square_right square color board <> None then
+      get_vacant square_right square color board :: squares
     else squares
   in
   let squares =
-    if get_vacant square_left square board <> None then
-      get_vacant square_left square board :: squares
+    if get_vacant square_left square color board <> None then
+      get_vacant square_left square color board :: squares
     else squares
   in
   squares
 
-let get_jumps_d sq (brd : t) clr func =
+(* [get_jumps_dir sq brd clr fun] gets the jump in a given direction
+   guided by function [func] for a piece of color [clr] on square [sq]
+   on board [brd]*)
+let get_jumps_dir sq (brd : t) clr func =
   (* Get next square in direction *)
-  let nxt_sq = func sq brd in
+  let nxt_sq = func sq clr brd in
   (* If we got something... *)
   if nxt_sq <> [] then
     (* Extract square from list *)
     let nxt_sq = List.nth nxt_sq 0 in
     (* Get 2 squares next in given direction from sq *)
-    let nxt_2 = func nxt_sq brd in
+    let nxt_2 = func nxt_sq clr brd in
     (* If we got something... *)
     if nxt_2 <> [] then
       (* Extract the square from the list *)
       let nxt_2 = List.nth nxt_2 0 in
       (* If the next square is occupied and the 2nd next square is empty*)
       if
-        get_vacant func sq brd = None
-        && get_vacant func nxt_2 brd <> None
+        get_vacant func sq clr brd = None
+        && get_vacant func nxt_2 clr brd <> None
         (* get the piece from the piece option *)
       then
         let pc_abv = Option.get nxt_sq.occupant in
@@ -172,12 +208,25 @@ let get_jumps_d sq (brd : t) clr func =
    represent all possible jumps avalible for the piece of color [clrf]
    on square [sq] on board [brd] *)
 let get_all_jumps sq brd clr =
-  let above = get_jumps_d sq brd clr square_above in
-  let left = get_jumps_d sq brd clr square_left in
-  let right = get_jumps_d sq brd clr square_right in
+  let above = get_jumps_dir sq brd clr square_above in
+  let left = get_jumps_dir sq brd clr square_left in
+  let right = get_jumps_dir sq brd clr square_right in
   above @ left @ right
 
-let where_move brd sq (st : State.state) =
+(* [get_all_vac_sq_dir acc sq clr brd func] is the list of squares that
+   are vacant in direction determined by seeking function [func] of
+   square [sq] given a piece of color [clr] occupying said sqaure [sq]
+   on board [brd]. Function is tail recursive with accumulator list
+   [acc]*)
+let rec get_all_vac_sq_dir acc sq clr brd func =
+  let next = get_vacant func sq clr brd in
+  match next with
+  | Some n_sqr -> get_all_vac_sq_dir (n_sqr :: acc) n_sqr clr brd func
+  | None -> acc
+
+let get_all_jumps_lady sq brd clr = failwith "f"
+
+let where_move brd sq =
   try
     let pc = Option.get sq.occupant in
     let pc_typ = pc.role in
@@ -190,12 +239,12 @@ let where_move brd sq (st : State.state) =
     else []
   with _ -> raise EmptyStartSquare
 
-let can_move square board (st : State.state) =
+let can_move square board (st : state) =
   let condition1 = not (check_if_occupied square) in
   if square.occupant <> None then
     let pc = Option.get square.occupant in
-    let condition2 = pc.color = State.get_turn st in
-    let condition3 = where_move board square st <> [] in
+    let condition2 = pc.color = st.turn in
+    let condition3 = where_move board square <> [] in
     condition1 && condition2 && condition3
   else false
 
@@ -308,10 +357,10 @@ let string_of_row r row =
     ("|" ^ string_of_int row)
     r
 
-let rec terminal_rep_string_helper t count =
-  match t with
+let rec terminal_rep_string_helper (t : t) count =
+  match t.board with
   | [] -> ""
-  | h :: t ->
+  | h :: _ ->
       string_of_row h count ^ "\n"
       ^ terminal_rep_string_helper t (count + 1)
 
@@ -324,5 +373,4 @@ let rec col_label_string count n =
   else " \n"
 
 let terminal_rep_string t count =
-  col_label_string count t.size
-  ^ terminal_rep_string_helper t.board count
+  col_label_string count t.size ^ terminal_rep_string_helper t count
