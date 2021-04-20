bb6d7e936f818b3011f31a2fa0f94ef337b32cb8
Nada Attia <nma46@cornell.edu>
2021-04-20 13:30:15 -0400

    Add images for white piece and empty square

--- gui.ml
+++ gui.ml
@@ -2,15 +2,22 @@ open Graphics
 open Png
 open Graphic_image
 
-let init_window =
-  open_graph "";
-  set_window_title "Dama";
-  let img = Png.load_as_rgb24 "images/black-w-background.png" [] in
+let display_image img_path x y =
+  let img = Png.load_as_rgb24 img_path [] in
   let graphics_img = Graphic_image.of_image img in
-  Graphics.draw_image graphics_img 0 0
+  Graphics.draw_image graphics_img x y
+
+let display_images () =
+  display_image "images/black-w-background.png" 0 0;
+  display_image "images/white-w-background.png" 0 82
+
+let init_window =
+  open_graph " 1000x800";
+  set_window_title "Dama"
 
 let rec loop () = loop ()
 
 let () =
   init_window;
+  display_images ();
   loop ()
