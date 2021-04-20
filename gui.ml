62d9f6a7e4cf53fb639680739edb3c75f712fef0
Nada Attia <nma46@cornell.edu>
2021-04-20 13:15:26 -0400

    Add image with background

--- gui.ml
+++ gui.ml
@@ -1,12 +1,11 @@
 open Graphics
 open Png
 open Graphic_image
-open Jpeg
 
 let init_window =
   open_graph "";
   set_window_title "Dama";
-  let img = Jpeg.load "images/black-piece.jpeg" [] in
+  let img = Png.load_as_rgb24 "images/black-w-background.png" [] in
   let graphics_img = Graphic_image.of_image img in
   Graphics.draw_image graphics_img 0 0
 
